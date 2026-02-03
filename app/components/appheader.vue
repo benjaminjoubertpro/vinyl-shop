98a8951713138e6baba14dc0b888cb852d717f35
Benjamin Joubert <benjaminjoubertpro@gmail.com>
2026-02-03 15:30:08 +0100

    Initial commit

---
+++ app/components/appheader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="flex justify-between items-center bg-black text-white px-4 py-4">
+            <div class="flex justify-center items-center gap-6">
+                <img class="w-14 h-auto" src="assets/img/logovinyl.png" alt="Logo">
+                <nav>
+                    <ul class="flex gap-4">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Produits</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="flex gap-4">
+                <a class="btn" href="#">Connexion</a>
+                <a class="btn" href="#">Inscription</a>
+            </div>
+        </div>
+    </header>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <appheader />
+</template>
